379eebbe1fd32418db955cf20871d2aaecdcc887
ruthinunes <ruthinunesdev@gmail.com>
2023-06-15 09:07:03 -0300

    Realocating List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import Form from "./components/Form/Form";
 import List from "./components/List/List";
+import data from "../data/tasks.json";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState(data);
+
   return (
     <>
       <div className="container">
-        <List />
+        <Form
+          onSubmit={(newTask) =>
+            setTasks([
+              ...tasks, //Setting id to timestamp to avoid duplicity
+              { id: new Date().getTime(), description: newTask.description },
+            ])
+          }
+        />
+
+        <List
+          tasks={tasks}
+          onDelete={(id) => setTasks(tasks.filter((task) => task.id !== id))}
+          onEdit={(id) => console.log(id)}
+        />
       </div>
     </>
   );
