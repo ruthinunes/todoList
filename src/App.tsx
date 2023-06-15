364076c96b1b0043a96872ac5b0e34b379c4236d
ruthinunes <ruthinunesdev@gmail.com>
2023-06-14 23:50:15 -0300

    Realocating Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Form from "./components/Form/Form";
 import List from "./components/List/List";
 
 function App() {
   return (
     <>
       <div className="container">
-        <Form />
         <List />
       </div>
     </>
