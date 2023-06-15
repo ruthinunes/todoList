e19cc9f326c4ff79c9638e42d15516b0c9f77f2f
ruthinunes <ruthinunesdev@gmail.com>
2023-06-14 23:51:20 -0300

    Realocating Form, creating add and delete function

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,29 +1,40 @@
+import Form from "../Form/Form";
 import ListBtns from "../ListBtns/ListBtns";
-import taskData from "../../../data/tasks.json";
+import   data from "../../../data/tasks.json";
 import { useState } from "react";
 
 const List = () => {
-  const [tasks, setTask] = useState(taskData);
+  const [tasks, setTasks] =   useState(data);
 
   return (
-    <div className="task-ctn">
-      <ul className="task-list">
-        {tasks.map((task) => (
-          <li className="task-list-item" key={task.id}>
-            <p>{task.description}</p>
-
-            <ListBtns
-              onDelete={() =>
-                setTask(tasks.filter((currentTask) => currentTask.id !== task.id))
-              }
-              onEdit={() => console.log(task.id)}
-            />
-          </li>
-        ))}
-      </ul>
-
-      <button className="clear-btn"> Clear All</button>
-    </div>
+    <>
+      <Form
+        onSubmit={(newTask) =>
+          setTasks([
+            ...tasks,
+            { description: newTask.description, id: tasks.length + 1 },
+          ])
+        }
+      />
+      <div className="task-ctn">
+        <ul className="task-list">
+          {tasks.map((task) => (
+            <li className="task-list-item" key={task.id}>
+              <p>{task.description}</p>
+              <ListBtns
+                onDelete={() =>
+                  setTasks(
+                    tasks.filter((currentTask) => currentTask.id !== task.id)
+                  )
+                }
+                onEdit={() => console.log(task.id)}
+              />
+            </li>
+          ))}
+        </ul>
+        <button className="clear-btn"> Clear All</button>
+      </div>
+    </>
   );
 };
 
