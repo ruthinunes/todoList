033dc118b23cb685ce71ae87a8f80672892f14d2
ruthinunes <ruthinunesdev@gmail.com>
2023-06-14 21:24:50 -0300

    removing data from app

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,11 @@ import Form from "./components/Form/Form";
 import List from "./components/List/List";
 
 function App() {
-  const tasks = [
-    { id: 1, description: "AAA" },
-    { id: 2, description: "BBB" },
-    { id: 3, description: "CCC" },
-    { id: 4, description: "DDD" },
-  ];
-
   return (
     <>
       <div className="container">
         <Form />
-        <List tasks={tasks} />
+        <List />
       </div>
     </>
   );
