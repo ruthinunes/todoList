f8be639fe82ff30bd57bcfb83506652cb31a5e73
ruthinunes <ruthinunesdev@gmail.com>
2023-06-15 09:07:58 -0300

    Realocating ListBtns

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,40 +1,30 @@
-import Form from "../Form/Form";
 import ListBtns from "../ListBtns/ListBtns";
-import   data from "../../../data/tasks.json";
-import { useState } from "react";
 
-const List = () => {
-  const [tasks, setTasks] =   useState(data);
+interface TaskProps {
+  id: number;
+  description: string;
+}
 
+interface ListProps {
+  tasks: TaskProps[];
+  onDelete: (id: number) => void;
+  onEdit: (id: number) => void;
+}
+
+const List = ({ tasks, onDelete, onEdit }: ListProps) => {
+  if (tasks.length === 0) return null;
   return (
-    <>
-      <Form
-        onSubmit={(newTask) =>
-          setTasks([
-            ...tasks,
-            { description: newTask.description, id: tasks.length + 1 },
-          ])
-        }
-      />
-      <div className="task-ctn">
-        <ul className="task-list">
-          {tasks.map((task) => (
-            <li className="task-list-item" key={task.id}>
-              <p>{task.description}</p>
-              <ListBtns
-                onDelete={() =>
-                  setTasks(
-                    tasks.filter((currentTask) => currentTask.id !== task.id)
-                  )
-                }
-                onEdit={() => console.log(task.id)}
-              />
-            </li>
-          ))}
-        </ul>
-        <button className="clear-btn"> Clear All</button>
-      </div>
-    </>
+    <div className="task-ctn">
+      <ul className="task-list">
+        {tasks.map((task) => (
+          <li className="task-list-item" key={task.id}>
+            <p>{task.description}</p>
+            <ListBtns id={task.id} onDelete={onDelete} onEdit={onEdit} />
+          </li>
+        ))}
+      </ul>
+      <button className="clear-btn"> Clear All</button>
+    </div>
   );
 };
 
