c51436154931cd5db28fbd1a43f8f8ac6bab14ae
ruthinunes <ruthinunesdev@gmail.com>
2023-06-17 21:33:52 -0300

    Removing edit function

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ function App() {
         <List
           tasks={tasks}
           onDelete={(id) => setTasks(tasks.filter((task) => task.id !== id))}
-          onEdit={(id) => console.log(id)}
           onClearAll={() => setTasks([])}
         />
       </div>
