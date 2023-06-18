75dd77662dfdc7a97657a6dddbe7f9c7f5f4c040
ruthinunes <ruthinunesdev@gmail.com>
2023-06-17 21:36:04 -0300

    Placing delete button on list component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,4 @@
-import ListBtns from "../ListBtns/ListBtns";
+import { FaTrash } from "react-icons/fa";
 
 interface TaskProps {
   id: number;
@@ -8,19 +8,21 @@ interface TaskProps {
 interface ListProps {
   tasks: TaskProps[];
   onDelete: (id: number) => void;
-  onEdit: (id: number) => void;
   onClearAll: () => void;
 }
 
-const List = ({ tasks, onDelete, onEdit, onClearAll }: ListProps) => {
+const List = ({ tasks, onDelete, onClearAll }: ListProps) => {
   if (tasks.length === 0) return null;
+
   return (
     <div className="task-ctn">
       <ul className="task-list">
         {tasks.map((task) => (
           <li className="task-list-item" key={task.id}>
             <p>{task.description}</p>
-            <ListBtns id={task.id} onDelete={onDelete} onEdit={onEdit} />
+            <button onClick={() => onDelete(task.id)}>
+              <FaTrash size={14} />
+            </button>
           </li>
         ))}
       </ul>
